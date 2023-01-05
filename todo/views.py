5e06a1e2384f45a9aa55104f7c04eea7d9dcdc33
YuriShepel <donbidonn@gmail.com>
2023-01-05 18:59:21 +0200

    Used linter and formater

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.forms import User
-from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.db import IntegrityError, Error
+from django.contrib.auth.forms import (AuthenticationForm, User, UserCreationForm)
+from django.db import IntegrityError
+from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
+
 from .forms import TodoForm
 from .models import Todo
 
